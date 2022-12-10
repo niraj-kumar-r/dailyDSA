5dbd8f4b63cc382b60817a88c45706f8853dce24
Niraj Kumar <nirajk23523@gmail.com>
2022-12-11 02:15:40 +0530

    greedy approach

--- Code/jumpGame.cpp
+++ Code/jumpGame.cpp
@@ -4,25 +4,12 @@ using namespace std;
 
 bool canJump(vector<int> &nums)
 {
-    vector<bool> arr(nums.size(), false);
-    arr[0] = true;
-    int n = nums.size();
-
-    for (int i = 0; i < n; i++)
+    int max_jump_index = 0;
+    for (int i = 0, n = nums.size(); max_jump_index >= i && max_jump_index < n; i++)
     {
-        if (arr[i])
-        {
-            for (int j = 1; i + j < n && j <= nums[i]; j++)
-            {
-                arr[i + j] = true;
-            }
-        }
-        if (arr[n - 1])
-        {
-            return true;
-        }
+        max_jump_index = i + nums[i] > max_jump_index ? i + nums[i] : max_jump_index;
     }
-    return arr[n - 1];
+    return max_jump_index >= nums.size() - 1;
 }
 
 int main()
