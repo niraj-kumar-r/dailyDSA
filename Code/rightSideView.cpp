602de634a03878273ce77b989de4611e909d8c2d
Niraj Kumar <nirajk23523@gmail.com>
2023-03-06 03:24:44 +0530

    another Try

--- Code/rightSideView.cpp
+++ Code/rightSideView.cpp
@@ -18,30 +18,36 @@ class Solution
 public:
     vector<int> rightSideView(TreeNode *root)
     {
-        vector<int> a;
+        vector<int> a, b, c;
         if (!root)
         {
             return a;
         }
         a.push_back(root->val);
-        do
+        b = rightSideView(root->left);
+        c = rightSideView(root->right);
+        a.insert(a.end(), c.begin(), c.end());
+        int d = c.size() - b.size();
+        if (d > 0)
         {
-            if (!root->right)
-            {
-                root = root->left;
-            }
-            else
-            {
-                root = root->right;
-            }
-            if (!root)
-            {
-                break;
-            }
-            a.push_back(root->val);
-
-        } while (root->right);
-
+            a.insert(a.end(), b.begin() + (b.size() - d), b.end());
+        }
         return a;
     }
 };
+
+int main()
+{
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->right = new TreeNode(5);
+    root->right->right = new TreeNode(4);
+    Solution s;
+    vector<int> a = s.rightSideView(root);
+    for (int i = 0; i < a.size(); i++)
+    {
+        cout << a[i] << " ";
+    }
+    return 0;
+}
